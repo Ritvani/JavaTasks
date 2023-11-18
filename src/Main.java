f01805557d20a573ba09db8815e8b3c59658a4e8
Ritvan Bicaku <ritivanbicaku@gmail.com>
2023-11-18 11:41:32 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Coding Dojo";
+        int myAge = 100;
+        String myTown = "Burbark";
+
+        System.out.println("My name is " + myName);
+        System.out.println("I am " + myAge + " years old");
+        System.out.println("My hometown is " + myTown + ", CA");
+    }
+}
